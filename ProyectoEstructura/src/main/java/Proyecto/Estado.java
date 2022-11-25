82f227051a007a9554cdd8aa361b737cbe643b3e
Gabriela Jiménez <mjimenez40125@ufide.ac.cr>
2022-11-24 21:06:07 -0600

    vehiculo

---
+++ ProyectoEstructura/src/main/java/Proyecto/Estado.java
@@ -0,0 +1,14 @@
+
+package Proyecto;
+
+/**
+ *
+ * @author Maria Gabriela
+ */
+public enum Estado {
+     Disponible,
+     Alquilado,
+     En_reparacion,
+     Fuera_Circulacion
+    
+}
--- ProyectoEstructura/src/main/java/Proyecto/Main.java
+++ ProyectoEstructura/src/main/java/Proyecto/Main.java
@@ -1,12 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package Proyecto;
 
 /**
  *
  * @author Juan Carlos
+ * @author Gabriela Jimenez 
  */
 public class Main {
 
