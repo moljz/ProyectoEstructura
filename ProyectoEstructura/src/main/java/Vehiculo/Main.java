f15eebc5bf7736e906ddc71d7a184d8ef5d88a30
Gabriela Jiménez <mjimenez40125@ufide.ac.cr>
2022-11-29 22:22:09 -0600

    Métodos Vehiculo
    Listo :) bye chiquis

--- ProyectoEstructura/src/main/java/Vehiculo/Main.java
+++ ProyectoEstructura/src/main/java/Vehiculo/Main.java
@@ -11,17 +11,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, este es el proyecto");
         
-        
-         Cola cola1 = new Cola();
-
-        cola1.encola(new Nodo(new Vehiculo()));
-        cola1.encola(new Nodo(new Vehiculo()));
-        cola1.encola(new Nodo(new Vehiculo()));
-        cola1.encola(new Nodo(new Vehiculo()));
-        cola1.encola(new Nodo(new Vehiculo()));
-        cola1.encola(new Nodo(new Vehiculo()));
-
-        System.out.println(cola1);
-    }
+         Lista laLista = new Lista();
+        //Llamamos al método inserta y como este pide un obj de tipo P tenemos que 
+        //crear un nuevo obj p y le pasamos los parámetros que este pide 
+        laLista.inserta(new Vehiculo());
+        laLista.inserta(new Vehiculo());
+        laLista.inserta(new Vehiculo());
+        laLista.inserta(new Vehiculo());
+        laLista.inserta(new Vehiculo());
+        laLista.inserta(new Vehiculo());
+        laLista.inserta(new Vehiculo());
+        //laLista.inserta();
+        System.out.print(laLista);
+     
     
+}
 }
--- ProyectoEstructura/src/main/java/Vehiculo/Nodo.java
+++ ProyectoEstructura/src/main/java/Vehiculo/Nodo.java
@@ -6,33 +6,29 @@ package Vehiculo;
  * @author Maria Gabriela
  */
 public class Nodo {
-    private Vehiculo dato;
-    private Nodo atras;
+    private Vehiculo dato; //donde se almacenará la info de la persona 
+    private Nodo next; //la referencia al siguiente obj de tipo Nodo que contiene una persona 
+
+    //Constructor que solo recibe persona, no se incluye el next 
 
     public Nodo(Vehiculo dato) {
         this.dato = dato;
     }
+    //Get and sets 
 
-    public Vehiculo getDato() {
-        return dato;
+    public Vehiculo getDato() { return dato; }
+
+    public void setDato(Vehiculo dato) {this.dato = dato;}
+
+    public Nodo getNext() {return next;}
+
+    public void setNext(Nodo next) {this.next = next;}
+   
+     @Override
+    public String toString() {
+        return "Nodo{" + "dato=" + dato + '}';
     }
-
-    public void setDato(Vehiculo dato) {
-        this.dato = dato;
-    }
-
-    public Nodo getAtras() {
-        return atras;
-    }
-
-    public void setAtras(Nodo atras) {
-        this.atras = atras;
-    }
-
     
     /* Editar el toString para mostrar toda la información del vehículo*/
-   @Override
-    public String toString() {
-        return "El valor es: " + String.valueOf(dato.getNumPlaca());
-    }
+   
 }
